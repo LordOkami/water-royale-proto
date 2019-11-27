21d2fc70d741bf0c3e4e35ca2ac1dfacd8ebf993
Luis Sebastián Huerta <luissebastianhuerta@gmail.com>
2019-11-27 21:57:04 +0100

    feat: improve follow behaviour

--- Assets/Scripts/FollowLeader/FollowBehaviour.cs
+++ Assets/Scripts/FollowLeader/FollowBehaviour.cs
@@ -4,14 +4,16 @@ public class FollowBehaviour : MonoBehaviour
 {
     public Transform player;
     public float zIndex = -6;
-    public Vector3 offset = new Vector3(1, 1, 0);
+    public Vector3 offset = new Vector3(0, 0, 0);
     public float speed = 1.0f;
 
     void FixedUpdate()
     {
         if (player != null)
         {
-            Vector3 lerp = Vector3.Lerp(transform.position, player.position + offset, Time.deltaTime * speed);
+            //Rigidbody2D playerRigidBody = player.GetComponent < Rigidbody2D>();
+            float distance = (player.position - transform.position).magnitude;
+            Vector3 lerp = Vector3.Lerp(transform.position, player.position + offset, Time.deltaTime * speed * (1 / distance));
             transform.position = new Vector3(lerp.x, lerp.y, zIndex);
         }
     }
