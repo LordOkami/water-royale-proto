242c42caaf99756acb5d46231e49976737d75a68
Luis Sebastián Huerta <luissebastianhuerta@gmail.com>
2019-12-17 17:30:54 +0100

    feat: add audio events and example audio assets
    * Also RangedFloat attribute was added

---
+++ Assets/Scripts/AudioEvent/SimpleAudioEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(menuName="Audio Events/Simple")]
+public class SimpleAudioEvent : AudioEvent
+{
+    public AudioClip[] clips;
+
+    [Header("Ranged Float With Editable Limits")]
+    [RangedFloat(0, 1, RangedFloatAttribute.RangeDisplayType.EditableRanges)]
+    public RangedFloat volume;
+
+    [Header("Ranged Float With Editable Limits")]
+    [RangedFloat(0, 1, RangedFloatAttribute.RangeDisplayType.EditableRanges)]
+    public RangedFloat pitch;
+
+    public override void Play(AudioSource source)
+    {
+        if (clips.Length == 0) return;
+        source.clip = clips[Random.Range(0, clips.Length)];
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
+    }
+}
