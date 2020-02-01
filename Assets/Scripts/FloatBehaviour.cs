9809c237b49a2b9ce351a5ab83f8d2ca5ec031d2
Luis Sebastián Huerta <luissebastianhuerta@gmail.com>
2020-02-01 11:31:40 +0100

    feat: add flotability script

---
+++ Assets/Scripts/FloatBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatBehaviour : MonoBehaviour
+{
+    float factor = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D hit)
+    {
+        if (hit.attachedRigidbody)
+        {
+
+            if (hit.transform.position.y < -1)
+            {
+                float distance = hit.transform.position.y + 1;
+                Debug.Log(distance);
+                Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+                rb.AddForce(Vector3.up * rb.mass * -distance * factor);
+            }
+           
+        }
+    }
+
+}
