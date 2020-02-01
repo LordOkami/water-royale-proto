bdc099bdf9ebbe1ce4be6d0fea1ef6809ce0b524
Marcos Pérez Ferro <djwmarcx@users.noreply.github.com>
2020-02-01 11:30:59 +0100

    Scales Great

--- Assets/IndividualGameController.cs
+++ Assets/IndividualGameController.cs
@@ -5,35 +5,58 @@ using UnityEngine;
 public class IndividualGameController : MonoBehaviour
 {
 
-    public int valveSpawnsPerSeconds = 3;
-    public int pixelsPerFrame = 10;
+    public float valveSpawnEverySeconds = 1f;
+    public float pixelsPerFrame = 0.01f;
     
     public int width = 480;
     public int height = 720;
 
-    private List<GameObject> actionables;
-        
-    private int step = 0;
-    private int currentOffset = 0;
+    public GameObject[] availableActionables;
+    private List<GameObject> currentActionables = new List<GameObject>();
+
+    private int maxActionablesPerIteration = 3;
+
+    
+    private GameObject gameContainer;
 
     void Awake()
     {
         // Uncommenting this will cause framerate to drop to 10 frames per second.
         // This will mean that FixedUpdate is called more often than Update.
         Application.targetFrameRate = 60;
-      
+        //availableActionables = Resources.LoadAll("Prefab/Valves", typeof(GameObject));
+
+        
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
+        gameContainer = new GameObject();
+        gameContainer.name = "Game container";
+        gameContainer.transform.localScale = new Vector2(width, height);
+        gameContainer.transform.parent = transform;
+        SpriteRenderer spriteRenderer = gameContainer.AddComponent<SpriteRenderer>();
 
-        GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        plane.transform.position = new Vector3(0, 0, 0);
-        plane.transform.localScale = new Vector3(width,0, height);
+
+
+        /*  spriteRenderer.color = Color.red;
+        
+        spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Square") as Sprite;*/
         
 
+
+        /*GameObject playerCameraGameObject = new GameObject("Player Camera");
+        playerCameraGameObject.transform.parent = gameContainer.transform;
+        Camera camera = playerCameraGameObject.AddComponent<Camera>();
+        
+        camera.orthographic = true;
+        camera.orthographicSize = 9;
+        camera.farClipPlane = 10;
+        playerCameraGameObject.transform.position = new Vector3(0, 0, -10);
+        */
+        StartCoroutine(CreateValve());
 
 
     }
@@ -47,9 +70,39 @@ public class IndividualGameController : MonoBehaviour
     // Increase the number of calls to FixedUpdate.
     void FixedUpdate()
     {
-        currentOffset = step * pixelsPerFrame;
-        step++;
-        Debug.Log(step);
+        
+        currentActionables.ForEach(go =>
+        {
+            go.transform.position = go.transform.position - new Vector3(0, pixelsPerFrame);
+        });
+
+
+
+    }
+
+#pragma warning disable IDE0051 // Quitar miembros privados no utilizados
+    IEnumerator CreateValve()
+#pragma warning restore IDE0051 // Quitar miembros privados no utilizados
+    {
+
+        while (true)
+        {
+            Debug.Log("Creating Valve interation");
+
+            GameObject newValve = Instantiate(availableActionables[Random.Range(0, availableActionables.Length - 1)]);
+
+            float valveWidth = newValve.GetComponent<SpriteRenderer>().size.x;
+            float valveHeight = newValve.GetComponent<SpriteRenderer>().size.y;
+
+            float randomX = Random.Range(valveWidth / 2, width - (valveWidth / 2))-(width/2);
+
+            newValve.transform.position = new Vector3(randomX, (height / 2) + valveHeight, 0);
+            newValve.transform.parent = gameContainer.transform;
+            
+            currentActionables.Add(newValve);
+
+            yield return new WaitForSeconds(valveSpawnEverySeconds);
+        }
     }
 
 
